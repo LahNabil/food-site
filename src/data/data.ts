bc8af3418b77039ea7b2eb962210241d0069cd25
Nabil <lahlalia.nabil@hotmail.com>
2024-09-18 19:35:16 +0100

    assgin data and ++hero section

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,3 +1,5 @@
+
+
 export const navs = [
     {
         id:1,
@@ -46,28 +48,56 @@ export const heroSlides = [
         id:1,
         bgImg: '/assets/photo1.jpg',
         title: 'photo1',
-        brief: 'jehj jkehka',
+        brief: 'Neat own nor she said see walk. And charm add green you these. Sang busy in this drew ye fine. At greater prepare musical so attacks as on distant. Improving age our her cordially intention. His devonshire sufficient precaution say preference middletons insipidity. Since might water hence the her worse. Concluded it offending dejection do earnestly as me direction. Nature played thirty all him.',
         link: ''
     },
     {
         id:2,
-        bgImg: '',
-        title: 'photo2',
-        brief: '',
+        bgImg: '/assets/photo2.jpg',
+        title: 'photo 2',
+        brief: 'Neat own nor she said see walk. And charm add green you these. Sang busy in this drew ye fine. At greater prepare musical so attacks as on distant. Improving age our her cordially intention. His devonshire sufficient precaution say preference middletons insipidity. Since might water hence the her worse. Concluded it offending dejection do earnestly as me direction. Nature played thirty all him.',
         link: ''
     },
     {
         id:3,
-        bgImg: '',
-        title: 'photo3',
-        brief: '',
+        bgImg: '/assets/photo3.jpg',
+        title: 'photo 3',
+        brief: 'Neat own nor she said see walk. And charm add green you these. Sang busy in this drew ye fine. At greater prepare musical so attacks as on distant. Improving age our her cordially intention. His devonshire sufficient precaution say preference middletons insipidity. Since might water hence the her worse. Concluded it offending dejection do earnestly as me direction. Nature played thirty all him.',
         link: ''
     },
     {
         id:4,
-        bgImg: '',
-        title: 'photo4',
-        brief: '',
+        bgImg: '/assets/photo4.jpg',
+        title: 'photo 4',
+        brief: 'Neat own nor she said see walk. And charm add green you these. Sang busy in this drew ye fine. At greater prepare musical so attacks as on distant. Improving age our her cordially intention. His devonshire sufficient precaution say preference middletons insipidity. Since might water hence the her worse. Concluded it offending dejection do earnestly as me direction. Nature played thirty all him.',
         link: ''
     },
+]
+
+export const postItems = [
+    {
+        id: 1,
+        img: '/assets/photo2.jpg',
+        category: 'Category 1',
+        date: 'date1',
+        title: 'title1',
+        brief: 'brief1',
+        author: 'author1',
+        avatar: 'avatar1',
+        type: false,
+        trending: false
+    },
+    {
+        id: 2,
+        img: '/assets/photo2.jpg',
+        category: 'Category 2',
+        date: 'date2',
+        title: 'title2',
+        brief: 'brief2',
+        author: 'author2',
+        avatar: 'avatar2',
+        type: false,
+        trending: false
+    },
+
 ]
--- src/sections/Recipes.tsx
+++ src/sections/Recipes.tsx
@@ -1,7 +1,36 @@
-import React from 'react'
+'use client';
+import React, {useState, useEffect} from 'react'
+import { useRouter } from 'next/navigation';
+//import './posts.css'
+import { postItems } from '@/data/data';
+import AOS from 'aos';
+import { PostItemOne } from '@/components/PostItemOne';
 
 export const Recipes = () => {
+  // const [items, setItems] = useState(postItems);
+
+  useEffect(()=>{
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: false,
+      mirror: false
+    })
+  }, []);
   return (
-    <div>Recipes</div>
-  )
+    <section id='posts' className='posts'>
+      <div className="container">
+        {
+
+          postItems.map(item=>(
+            <PostItemOne key={item.id} large={false} item={item} />
+            // <p key={item.id}>
+            //   {item.title}
+            // </p>
+          ))
+        }
+      </div>
+    </section>
+  );
+  
 }
