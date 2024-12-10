33f8570fdff1f0280afd03614c75ece023906921
Nabil <lahlalia.nabil@hotmail.com>
2024-12-10 18:12:39 +0100

    static sitemap

--- next-sitemap.js
+++
@@ -1,7 +0,0 @@
-// next-sitemap.js
-module.exports = {
-    siteUrl: 'https://www.fastcookiteasy.com', // Replace with your domain
-    generateRobotsTxt: false, // Optional: To generate robots.txt
-    sitemapSize: 7000, // Optional: You can split sitemaps if you have a large site
-  };
-  
