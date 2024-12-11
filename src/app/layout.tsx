ed7e7cbf28699fe466272aef5041561008f5bc4d
Nabil <lahlalia.nabil@hotmail.com>
2024-12-11 13:54:25 +0100

    tt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
+import { EB_Garamond } from "next/font/google";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'aos/dist/aos.css';
 import "./variables.css";
 import "./globals.css";
-import { GoogleTagManager } from '@next/third-parties/google' 
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+import Head from "next/head";
+import Script from "next/script";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
-
+const ebGaramond = EB_Garamond({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: "Cook it Easy",
@@ -20,17 +25,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <GoogleTagManager gtmId="GTM-WBT745PF" />
-      <body>
-      <noscript>
-        <iframe 
-        src="https://www.googletagmanager.com/ns.html?id=GTM-WBT745PF"
-        height="0"
-        width="0"
-        style={{ display: "none", visibility: "hidden" }}>
-        </iframe>
-      </noscript>
-        {children}</body>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta name="google-adsense-account" content={process.env.NEXT_PUBLIC_ADSENSE_PID} />
+      </Head>
+      
+      <body className={ebGaramond.className}>
+        <Header />
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_PID}`}
+          crossOrigin="anonymous"
+        />
+        {children}
+        {/* <CookieBanner/> */}
+        <Footer />
+      </body>
+      <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string}/>
     </html>
   );
 }
