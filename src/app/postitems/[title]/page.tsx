b397696c7d98e7814291b55cb3a60859f0d17605
Nabil <lahlalia.nabil@hotmail.com>
2024-12-15 15:33:22 +0100

    fixing items per title

---
+++ src/app/postitems/[title]/page.tsx
@@ -0,0 +1,167 @@
+  "use client";
+
+  import { postItems } from '@/data/data';
+  import React, { useState, useEffect} from 'react';
+  import './style.css';
+  import Image from 'next/image';
+  import { SidePostItem } from '@/components/SidePostItem';
+  import { useParams } from 'next/navigation';
+
+
+  interface Post {
+    id: number;
+    img: string;
+    category: string;
+    comment: string[];
+    title: string;
+    brief: string;
+    ingredients: string[];
+    preptime: number;
+    type: boolean;
+    trending: boolean;
+  }
+
+  const PostItem = () => {
+    const { title } = useParams(); 
+    // const postId = id ? Number(id) : null;
+    const [item, setItem] = useState<Post | null>(null);
+    const [items] = useState(postItems);
+    
+    const tabsData= [
+      {id: 1, name: 'popular', active: true},
+      {id: 2, name: 'trending', active: false}
+    ]
+    const[tabs, setTabs]= useState(tabsData);
+
+    const handleTabActive = (id:number):void=>{
+      setTabs(tabsData.map(tab=>{
+          tab.active= false;
+          if(tab.id === id) tab.active = true;
+          return tab;
+      }))
+    };
+
+    useEffect(() => {
+      if(typeof title === 'string'){
+        const decodedTitle = title.replace(/-/g, ' ');
+        const foundItem = postItems.find(post =>
+           post.title.toLowerCase() === decodedTitle.toLowerCase()); // Chercher le post avec cet ID
+      if (foundItem) {
+        setItem(foundItem);
+      }
+      }
+    }, [title]);
+
+    if (!item) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <main id="main" style={{ margin: '100px' }}>
+        <section className="single-post-content">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-9 post-content">
+                <div className="single-post">
+                  <div className="post-meta">
+                    <span className="date">{item.category}</span>
+                  </div>
+                  <h1 className="mb-5">{item.title}</h1>
+                  <p>
+                      <span className="firstcharacter">
+                          {item.brief && item.brief.charAt(0)}
+                      </span>
+                      {item.brief && item.brief.substring(1)}
+                  </p>
+                  <figure className='my-4'>
+                      <Image height={500} width={820} src={item.img} alt='photo_recipe' className='img-fluid' />
+                    
+                  <figcaption className='fig_caption'>
+                    <div className="infos_comment">
+                      <h2>Steps:</h2>
+                      <ol>
+                      {item.comment.map((step, index)=>(
+                        <li key={index}>{step}</li>
+                      ))}
+                      </ol>
+                    </div>
+                    <div className="infos_ingrédients">
+                    <h2>Ingredients:</h2>
+                    <ol>
+                    {item.ingredients.map((ingredient, index)=>(
+                      <li key={index}>{ingredient}</li>
+
+                    ))}
+                    </ol>
+                    </div>
+                    <div className="infos_duration">
+                      Duration: {item.preptime} min
+                    </div>
+                    
+                </figcaption>
+                </figure>
+                </div>
+              </div>
+              <div className="col-md-3">
+                  <div className="aside-block">
+                      <ul className='nav nav-pills custom-tab-nav mb-4'>
+                      {tabs.map(tab=>(
+                          <li className='nav-item' key={tab.id}>
+                          <button
+                          className={`nav-link ${
+                              tab.active ? 'active' : undefined
+                          }`}
+                          onClick={()=> handleTabActive(tab.id)}
+                          >{tab.name}</button>
+                          </li>
+                      ))}
+                      </ul>
+                      <div className="tab-content">
+                          <div className={`tab-pane fade ${
+                              tabs[0].active ? 'show active' : ''
+                          }`}>
+                              {items.slice(0,6)
+                              .map(item=>(
+                                  <SidePostItem key={item.id} item={item}/>
+                              ))
+                              }
+                          </div>
+                          <div className={`tab-pane fade ${
+                              tabs[1].active ? 'show active' : ''
+                          }`}>
+                              {items
+                              .filter(item=> item.trending)
+                              // .slice(6,12)
+                              .map(item=>(
+                                  <SidePostItem key={item.id} item={item}/>
+                              ))
+                              }
+                          </div>
+                      </div>
+                  </div>
+                  <div className="aside-block">
+                      <h3 className="aside-title">Video</h3>
+                      <div className="video-post">
+                          <a
+                          target='_blank'
+                          href="https://www.youtube.com/watch?v=Kes2fk-Nuwo&ab_channel=GordonRamsay"
+                          className='link-video'>
+                          <span className="bi-play-fill"></span>
+                          <Image
+                          height={100} width={100}
+                          src='/assets/photo3.jpg'
+                          alt="photo_video_recipe"
+                          className='img-fluid'
+                          />
+                          </a>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    );
+  };
+
+  export default PostItem;
--- src/components/PostItemOne.tsx
+++ src/components/PostItemOne.tsx
@@ -22,7 +22,7 @@ export const PostItemOne = ({large, item}:{
   return (
     
     <div className={`post-entry-1 ${large ? 'lg' : undefined}`}>
-        <Link href={`postitems/${item.id}`}>
+        <Link href={`postitems/${item.title.replace(/\s+/g, '-')}`}>
             <Image src={`${item.img}`} alt='recipe_food' width={290} height={100} className='img-fluid'/>
         </Link>
         <div className="post-meta">
