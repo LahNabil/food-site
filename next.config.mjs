99fa090ab9823041cfe76b548b996996372176f6
Nabil <lahlalia.nabil@hotmail.com>
2025-01-06 13:16:08 +0100

    working on perfomance lighthouse

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-import withBundleAnalyzer from '@next/bundle-analyzer';
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  
-};
+const nextConfig = {};
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
