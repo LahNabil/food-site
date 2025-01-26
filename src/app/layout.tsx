2c8ec0bd2a0a2e58e39cb88c9b811d9ffbb4693c
Nabil <lahlalia.nabil@hotmail.com>
2025-01-26 16:31:22 +0100

    adding pinterest ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,7 @@ export default function RootLayout({
       <Head>
         <link rel="icon" href="/favicon.ico"/>
         <meta name="google-adsense-account" content={process.env.NEXT_PUBLIC_ADSENSE_PID} />
+        <meta name="p:domain_verify" content={process.env.NEXT_PUBLIC_PINTEREST_VERIFY_ID}/>
       </Head>
       <body className={ebGaramond.className}>
         <noscript>
