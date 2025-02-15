ba3cd820250ffbe624678b3a35366ff5096cbcc7
Nabil <lahlalia.nabil@hotmail.com>
2025-02-15 22:30:14 +0100

    adding schema in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,9 +18,36 @@ export default function Home() {
     "name": "Cook it Easy",
     "description": "We are passionate about creating simple, delicious recipes for everyone to enjoy.",
     "url": "https://www.fastcookiteasy.com/",
+    "logo": "https://www.fastcookiteasy.com/assets/fastcookiteasy_logo.jpg",
     "sameAs": [
       "https://www.facebook.com/people/Cook-It-Easy/61566508671453/",
+      "https://www.pinterest.com/cookiteasy_/"
     ]
+  };
+  const webpageSchema = {
+    "@context": "https://schema.org",
+    "@type": "WebPage",
+    "url": "https://www.fastcookiteasy.com/",
+    "name": "Cook it Easy: Fast, Simple, and Delicious Recipes for Every Day",
+    "description": "Discover delicious and easy-to-make recipes for every occasion.",
+    "image": "https://www.fastcookiteasy.com/assets/about_image_ab.png"
+  };
+  const articleSchema = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "headline": "Cook It Easy: Fast, Simple, and Delicious Recipes for Every Day",
+    "description": "Explore our top 10 quick and easy recipes perfect for busy weeknights.",
+    "image": "https://www.fastcookiteasy.com//assets/kale-chicken-salade.webp",
+    "publisher": {
+      "@type": "Organization",
+      "name": "Cook it Easy",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://www.fastcookiteasy.com/assets/fastcookiteasy_logo.jpg"
+      }
+    },
+    "datePublished": "2024-12-01",
+    "dateModified": "2024-12-01"
   };
   return (
     <div id="main">
@@ -33,6 +60,18 @@ export default function Home() {
         id="organization-schema"
         type="application/ld+json"
         dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationSchema) }}
+      />
+      <Script
+        id="webpage-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(webpageSchema) }}
+      />
+
+      {/* Article Schema */}
+      <Script
+        id="article-schema"
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(articleSchema) }}
       />
       <h1 className='main-title'>Cook It Easy: Fast, Simple, and Delicious Recipes for Every Day</h1>
       <Hero/>
